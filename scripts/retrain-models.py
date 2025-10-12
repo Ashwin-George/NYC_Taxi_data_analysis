13da7e3adb8414adebbe025ff9ff1bd30c4b0545
Ashwin George <85630977+Ashwin-George@users.noreply.github.com>
2025-10-12 14:08:31 +0530

    changing the data_key for test purposes to csv

--- scripts/retrain-models.py
+++ scripts/retrain-models.py
@@ -13,7 +13,7 @@ from src.utils import download_from_s3, upload_to_s3
 # BUCKET = os.getenv("S3_BUCKET")
 BUCKET="nyc.archive.data.storage"
 PREFIX = os.getenv("S3_PREFIX", "models/")
-DATA_KEY = os.getenv("DATA_KEY", "data/nyc_sample_small.csv")
+DATA_KEY = os.getenv("DATA_KEY", "data/sample_retrain_check.csv")
 METADATA_KEY = os.path.join(PREFIX, "metadata.json")
 RETRAIN_THRESHOLD = 50000  # rows to trigger retraining
 
@@ -35,7 +35,7 @@ def save_metadata(row_count):
 
 def retrain_if_needed():
     # 1. Download latest dataset
-    local_file = "latest_data.parquet"
+    local_file = "latest_data.csv"
     download_from_s3(BUCKET, DATA_KEY, local_file)
     # df = pd.read_parquet(local_file)
     df=pd.read_csv(local_file)
